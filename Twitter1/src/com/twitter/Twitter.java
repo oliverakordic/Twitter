8a13d26df3b8fcc7648367c4fe522cc32951f9f0
oliverakordic <oljaitacka@gmail.com>
2016-04-11 06:02:57 +0200

    Napisana je dokumentacija za klasu Twitter, postavljeno da git ignorice doc file.

--- Twitter1/src/com/twitter/Twitter.java
+++ Twitter1/src/com/twitter/Twitter.java
@@ -1,21 +1,46 @@
 package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * 	
+ * @author Olivera
+ *
+ */
 public class Twitter {
+	/**
+	 * lista poruka tipa TwitterPoruka.
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-	
+	/**
+	 * Metoda vraca listu sa porukama tipa TwitterPoruka
+	 * @return poruke
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){
 		return poruke;
 	}
+	/**
+	 * Metoda unosi poruku tipa TwitterPoruka u listu sa porukama tog tipa. 
+	 * @param korisnik
+	 * @param poruka
+	 * @throws java.lang.RuntimeException Metoda baca izuzetak u slucaju da joj se prosledi null za vrednost korisnik.
+	 */
 	public void unesi(String korisnik, String poruka) {
+		if(korisnik==null)
+			throw new RuntimeException("Korisnik ne moze biti null");
 		//Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
-		tp.setKorisnik("korisnik");
+		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 		//Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
+	/**
+	 * Metoda pretrazuje poruke koje sadrze tag koji joj se prosledjuje i vraca niz sa porukama koje sadrze taj tag.
+	 * @param maxBroj
+	 * @param tag
+	 * @return rezultat
+	 * @throws java.lang.RuntimeException Izuzetak se baca u slucaju da je tag null ili prazan String.
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag==null || tag == "")
 			throw new RuntimeException("Morate uneti tag");
@@ -34,11 +59,13 @@ public class Twitter {
 		for (int i = 0; i < poruke.size(); i++)
 			if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
 				if (brojac < maxBroj){
-					rezultat[brojac+1]=poruke.get(i);
+					rezultat[brojac]=poruke.get(i);
 					brojac++;
 				}
 				else break;
 		return rezultat;
-	}
-} 
+		}
+	} 
+
+
 
